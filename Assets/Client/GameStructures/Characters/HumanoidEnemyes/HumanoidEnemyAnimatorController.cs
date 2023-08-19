55086784b1f2b9c26e8a21aaa3ea92220b3696ea
Фёдор Сазонов <fedoter1995@gmail.com>
2023-08-19 23:51:50 +0300

    Added blood splatter VFX

--- Assets/Client/GameStructures/Characters/HumanoidEnemyes/HumanoidEnemyAnimatorController.cs
+++ Assets/Client/GameStructures/Characters/HumanoidEnemyes/HumanoidEnemyAnimatorController.cs
@@ -5,15 +5,31 @@ namespace SpaceTraveler.GameStructures.Characters.HumanoidEnemyes
 {
     public class HumanoidEnemyAnimatorController : AnimatorController
     {
+        [SerializeField]
+        private ParticleSystem _bloodParticles;
+
+
         private int IntHurt = Animator.StringToHash("Hurt");
         private int IntDeath = Animator.StringToHash("Death");
+
+
+        public void Initialize()
+        {
+        }
+
         public void TakeHitAnimation()
         {
+            _bloodParticles.Play();
             SetTrigger(IntHurt);
+
         }
         public void DeathAnimation()
         {
             SetTrigger(IntDeath);
+        }
+        public void OnParticleSystemStopped()
+        {
+            _bloodParticles.Stop();
         }
     }
 }
